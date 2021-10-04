faf76dacbcbc5b55d230fa371e7a1250c64cf1bc
smirzoavliyoev <76482536+smirzoavliyoev@users.noreply.github.com>
2021-10-04 19:35:53 +0500

    use "generator" for fixing bug with dead lock

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"math/rand"
+	"sync"
 )
 
 type Key struct {
@@ -11,22 +12,34 @@ type Key struct {
 }
 
 var CH = make(chan Key)
+var wg = &sync.WaitGroup{}
 
 func main() {
 	A := make([]int, 800)
 	fillNumbers(A)
-	go searchRange(A, 0, 200, "Go1")
-	go searchRange(A, 200, 400, "Go2")
-	go searchRange(A, 400, 600, "Go3")
-	go searchRange(A, 600, 800, "Go4")
-
+	
+	go func () {
+		
+		wg.Add(4)
+		
+		go searchRange(wg, A, 0, 200, "Go1")
+		go searchRange(wg, A, 200, 400, "Go2")
+		go searchRange(wg, A, 400, 600, "Go3")
+		go searchRange(wg, A, 600, 800, "Go4")
+		
+		wg.Wait()
+		
+		close(CH)
+	}()
 
 	for i := range CH {
 		fmt.Println(i)
 	}
 }
 
-func searchRange(A []int, l int, r int, name string) {
+func searchRange(wg *sync.WaitGroup, A []int, l int, r int, name string) {
+	defer wg.Done()
+	
 	for i := l; i < r; i++ {
 		if A[i]%2 == 1 {
 			val := Key{
